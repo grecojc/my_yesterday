9bb3bfd156d97231b30bc1ea9e7a94cd72257aa9
grecojc <juliencgreco@gmail.com>
2014-12-04 11:45:20 -0500

    add tweets controller view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'trends#index'
   resources :trends
+  resources :tweets
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
