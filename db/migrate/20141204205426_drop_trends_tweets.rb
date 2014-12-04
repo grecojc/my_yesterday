799aebccbe3209941bd7910d9544a974782bf842
grecojc <juliencgreco@gmail.com>
2014-12-04 16:44:27 -0500

    changes

--- db/migrate/20141204205426_drop_trends_tweets.rb
+++
@@ -1,5 +0,0 @@
-class DropTrendsTweets < ActiveRecord::Migration
-  def change
-  	drop_table :trend_tweets
-  end
-end
--- db/migrate/20141204205551_drop_trend_tweets.rb
+++
@@ -1,5 +0,0 @@
-class DropTrendTweets < ActiveRecord::Migration
-  def change
-  	drop_table :trends_tweets
-  end
-end
