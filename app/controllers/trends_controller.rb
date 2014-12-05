6231e824e99fca62df84685b66333947b04e8715
grecojc <juliencgreco@gmail.com>
2014-12-05 16:56:35 -0500

    date

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,6 +1,6 @@
 class TrendsController < ApplicationController
 	def index
 		@trends = Trend.where("created_at >= ? and created_at < ?", Date.today, Time.now)
-		@date = Date.today.prev_day.strftime('%a' ' ' '%B' ' ' '%d' ' ' '%Y')
+		@date = Date.today.strftime('%a' ' ' '%B' ' ' '%d' ' ' '%Y')
 	end
 end
