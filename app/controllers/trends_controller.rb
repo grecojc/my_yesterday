5f14b154c4f6f48797996a71ae389f889a018846
grecojc <juliencgreco@gmail.com>
2014-12-06 15:58:55 -0500

    format date

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,6 +1,6 @@
 class TrendsController < ApplicationController
 	def index
 		@trends = Trend.where("created_at >= ? and created_at < ?", DateTime.now.in_time_zone("EST").to_date, Time.now.in_time_zone("EST"))
-		@date = DateTime.now.in_time_zone("EST").to_date
+		@date = DateTime.now.in_time_zone("EST").to_date.strftime('%a' ' ' '%B' ' ' '%d' ' ' '%Y')
 	end
 end
