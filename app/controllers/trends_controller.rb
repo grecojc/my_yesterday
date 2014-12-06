19c93f112469a6964996d9109f06f94f965aef6a
grecojc <juliencgreco@gmail.com>
2014-12-06 16:17:54 -0500

    test adding yesterday option

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -2,5 +2,6 @@ class TrendsController < ApplicationController
 	def index
 		@trends = Trend.where("created_at >= ? and created_at < ?", DateTime.now.in_time_zone("EST").to_date, Time.now.in_time_zone("EST"))
 		@date = DateTime.now.in_time_zone("EST").to_date.strftime('%a' ' ' '%B' ' ' '%d' ' ' '%Y')
+		@trends_yesterday = Trend.where("created_at >= ? and created_at < ?", DateTime.now.in_time_zone("EST").to_date.prev_day, DateTime.now.in_time_zone("EST").to_date)
 	end
 end
