5f0fc5b44a545ca5ac4df00cb8479c44e0480b71
grecojc <juliencgreco@gmail.com>
2014-12-07 15:23:16 -0500

    add newssource image to trend index view

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -2,5 +2,6 @@ class TrendsController < ApplicationController
 	def index
 		@trends = Trend.where("created_at >= ? and created_at < ?", Date.today.in_time_zone("EST"), Time.now.in_time_zone("EST"))
 		@date = DateTime.now.in_time_zone("EST").to_date.strftime('%a' ' ' '%B' ' ' '%d' ' ' '%Y')
+		@newssource = NewsSource.all
 	end
 end
