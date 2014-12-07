c44b5fe07ac9edd3cff9fe6799ab7ae76597f365
grecojc <juliencgreco@gmail.com>
2014-12-07 14:56:22 -0500

    test

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,6 +1,6 @@
 class TrendsController < ApplicationController
 	def index
-		@trends = Trend.where("created_at >= ? and created_at < ?", DateTime.now.prev_day, DateTime.now)
+		@trends = Trend.where("created_at >= ? and created_at < ?", DateTime.now.prev_day.to_date, DateTime.now.to_date)
 		@date = DateTime.now.in_time_zone("EST").to_date.strftime('%a' ' ' '%B' ' ' '%d' ' ' '%Y')
 	end
 end
