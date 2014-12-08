445bf6413c2b0c30d987f53dfdeaaf0758f3a0bd
grecojc <juliencgreco@gmail.com>
2014-12-07 19:37:23 -0500

    fix

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,6 +1,6 @@
 class TrendsController < ApplicationController
 	def index
-		@trends = Trend.all.limit(10)
+		@trends = Trend.all
 		@date = DateTime.now.in_time_zone("EST").to_date.strftime('%a' ' ' '%B' ' ' '%d' ' ' '%Y')
 		@newssources = NewsSource.all
 	end
