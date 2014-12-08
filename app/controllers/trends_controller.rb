8a4d6fc6d27611cebd02d10b5212ca65118828d3
grecojc <juliencgreco@gmail.com>
2014-12-07 19:20:30 -0500

    test

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,6 +1,6 @@
 class TrendsController < ApplicationController
 	def index
-		@trends = Trend.all
+		@trends = Trend.where("created_at >= ? and created_at < ?", Date.today, Time.now)
 		@date = DateTime.now.in_time_zone("EST").to_date.strftime('%a' ' ' '%B' ' ' '%d' ' ' '%Y')
 		@newssources = NewsSource.all
 	end
