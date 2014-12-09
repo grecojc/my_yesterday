15dc79b09a183d0ceb30e25316cf77cbca22fb71
grecojc <juliencgreco@gmail.com>
2014-12-08 22:27:22 -0500

    order by created_at descending

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,6 +1,6 @@
 class TrendsController < ApplicationController
 	def index
-		@trends = Trend.where("created_at >= ?", (Time.now.ago(5 * 60 * 60).to_date.to_time.advance :hours => 5))
+		@trends = Trend.where("created_at >= ?", (Time.now.ago(5 * 60 * 60).to_date.to_time.advance :hours => 5)).order(created_at: :desc)
 		@date = Time.now.in_time_zone("EST").to_date.strftime('%A' ' ' '%B' ' ' '%m' ' ' '%Y')
 		@newssources = NewsSource.all
 	end
