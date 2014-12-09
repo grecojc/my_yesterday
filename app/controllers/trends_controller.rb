1ce7a7aeaf1a7cb2e5d821bc051bbc0f50621717
grecojc <juliencgreco@gmail.com>
2014-12-08 20:26:52 -0500

    create trend pages

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,7 +1,13 @@
 class TrendsController < ApplicationController
 	def index
 		@trends = Trend.where("created_at >= ?", (Time.now.ago(5 * 60 * 60).to_date.to_time.advance :hours => 5))
-		@date = Time.now.in_time_zone("EST").to_date.strftime('%a' ' ' '%B' ' ' '%d' ' ' '%Y')
+		@date = Time.now.in_time_zone("EST").to_date.strftime('%A' ' ' '%B' ' ' '%m' ' ' '%Y')
+		@newssources = NewsSource.all
+	end
+
+	def show
+		@trend = Trend.find params[:id]
+		@date = Time.now.in_time_zone("EST").to_date.strftime('%A' ' ' '%B' ' ' '%m' ' ' '%Y')
 		@newssources = NewsSource.all
 	end
 end
